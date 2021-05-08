dbee587bf4b9d1b2586575e153f29079cc4a938c
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2021-05-08 16:01:10 -0500

    Create image scraper.py

---
+++ image scraper.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+url = "insert url here "
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+
+images = soup.find_all("img")
+number = 0
+
+for image in images:
+    image_src = image["src"]
+print(image_src)
+urllib.request.urlretrieve(image_src)
+number += 1
